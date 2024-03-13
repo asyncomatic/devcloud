c79fe1efb598814f2b3e20a2bb47297669ac2acf
JC Cormier <D9077C3D19645573@gmail.com>
2024-03-13 08:48:38 -0300

    Update table for state

--- config/postgres/init/config.sql
+++ config/postgres/init/config.sql
@@ -2,8 +2,8 @@ CREATE TABLE jobs (
                        id BIGSERIAL PRIMARY KEY,
                        delay INT,
                        queue TEXT,
-                       team_id BIGINT,
-                       user_id BIGINT,
-                       description TEXT,
-                       payload TEXT
+                       class TEXT,
+                       method TEXT,
+                       retry_count INT,
+                       state TEXT
 );
